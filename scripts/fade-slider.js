32e9f155cace009255444ef12b854894b2b7bca9
dEnis-ponti <delavaifag@gmail.com>
2021-11-10 15:55:43 +0300

    initional commit

---
+++ scripts/fade-slider.js
@@ -0,0 +1,22 @@
+(function() {
+    
+    const slides = document.querySelectorAll('.fade-slider__item');
+    const activeClass = "fade-slider__item--visible";
+    let index = 0;
+
+    console.log(slides[3]);
+
+    setInterval(function() {
+        slides[index].classList.remove(activeClass);
+        index++;
+        
+        if (index + 1 > slides.length) {
+            index = 0;
+        }
+        
+        slides[index].classList.add(activeClass);
+        console.log(index);
+
+    }, 5000);
+
+}())
